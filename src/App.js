f0ab63a7f9ca4ef1319e01f464ee1c5198d4da0d
rhealavella <rmdeosaran@gmail.com>
2023-08-23 20:50:33 -0400

    Added Bootstrap to repo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <div>
+      <div className="container">
+        <h1>Weather App</h1>
         <footer>
           This project was desgined and created by Rhea La Vella and is{" "}
           <a href="https://github.com/rhealavella/react-weather-app">
