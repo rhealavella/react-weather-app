cbab86ad69377589e3afb8f612ee8828375ff8e0
rhealavella <rmdeosaran@gmail.com>
2023-08-23 20:42:41 -0400

    Added GitHub Repo link to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div>
+        <footer>
+          This project was desgined and created by Rhea La Vella and is{" "}
+          <a href="https://github.com/rhealavella/react-weather-app">
+            open-sourced
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
