0b6dfee7e4a3eeb37444ec85f0c20704a275d6f0
rhealavella <rmdeosaran@gmail.com>
2023-09-02 00:00:34 -0400

    Added forecast API to WeatherForecast component

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,7 +1,17 @@
 import React from "react";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  let key = "e9ebt40ac8468b03ff07a7b93c22oc3b";
+  let longitude = props.coordinates.longitude;
+  let latitude = props.coordinates.latitude;
+  let url = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${key}&units=metric`;
+  axios.get(url).then(handleResponse);
   return (
     <div className="WeatherForecast">
       <div className="row">
