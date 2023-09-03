bb2a47e8b6b42b264ff67ed44cc29f2db01563f4
rhealavella <rmdeosaran@gmail.com>
2023-09-02 20:59:44 -0400

    Fixed Weather forecast search engine integration issue

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -10,38 +10,36 @@ export default function WeatherForecast(props) {
   useEffect(() => {
     setLoaded(false);
   }, [props.coordinates]);
-}
 
-function handleResponse(response) {
-  console.log(response.data);
-  setForecast(response.data.daily);
-  setLoaded(true);
-}
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
 
-if (loaded) {
-  return;
-  <div>
-    <div className="WeatherForecast">
-      <div className="row">
-        {forecast.map(function (dailyForecast, index) {
-          if (index < 5) {
-            return (
-              <div className="col" key={index}>
-                <WeatherForecastDay data={dailyForecast} />
-              </div>
-            );
-          }
-        })}
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
+        </div>
       </div>
-    </div>
-  </div>;
-} else {
-  let key = "e9ebt40ac8468b03ff07a7b93c22oc3b";
-  let longitude = props.coordinates.longitude;
-  let latitude = props.coordinates.latitude;
-  let url = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${key}&units=metric`;
+    );
+  } else {
+    let key = "e9ebt40ac8468b03ff07a7b93c22oc3b";
+    let longitude = props.coordinates.longitude;
+    let latitude = props.coordinates.latitude;
+    let url = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${key}&units=metric`;
 
-  axios.get(url).then(handleResponse);
+    axios.get(url).then(handleResponse);
 
-  return null;
+    return null;
+  }
 }
